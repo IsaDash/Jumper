33a3a8707fb180c260b2e24394543026d6bac01d
Isa Dash <isadash21@gmail.com>
2016-10-20 17:29:27 -0700

    Update JumperRunner.java

--- JumperRunner.java
+++ JumperRunner.java
@@ -1,24 +1,36 @@
+import java.awt.Color;
+
+import info.gridworld.actor.Actor;
 import info.gridworld.actor.ActorWorld;
 import info.gridworld.actor.Bug;
 import info.gridworld.actor.Rock;
+import info.gridworld.grid.Location;
 import info.gridworld.actor.Flower;
 
+
 /**
- *  TODO Write a one-sentence summary of your class here.
- *  TODO Follow it with additional details about its purpose, what abstraction
- *  it represents, and how to use it.
+ * TODO Write a one-sentence summary of your class here. TODO Follow it with
+ * additional details about its purpose, what abstraction it represents, and how
+ * to use it.
  *
- *  @author  TODO Name(s)
- *  @version TODO DATE
- *  @author  Period: TODO
- *  @author  Assignment: GridWorld_Part3_Jumper
+ * @author Eric Fu
+ * @version 10/20/16
+ * @author Period: 3
+ * @author Assignment: GridWorld_Part3_Jumper
  *
- *  @author  Sources: TODO
+ * @author Sources: BugRunner
  */
 public class JumperRunner
 {
     public static void main( String[] args )
     {
-        // TODO complete main
+        ActorWorld world = new ActorWorld();
+        world.add(new Jumper());
+        world.add(new Rock());
+        world.add(new Bug());
+        world.add(new Flower());
+        world.show(); 
+        
+
     }
 }
